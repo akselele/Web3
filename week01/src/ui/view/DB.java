fc4dec8969887239bd03d2a3f5d2faa9515d1790
akselele <axelelexd@gmail.com>
2019-10-02 14:03:18 +0200

    Labo 2 beide stories afgewerkt (SQL printen en sql toevoegen en printen)

--- week01/src/ui/view/DB.java
+++ week01/src/ui/view/DB.java
@@ -9,6 +9,8 @@ import java.util.Properties;
 
 import domain.model.Person;
 
+import javax.swing.*;
+
 public class DB {
     public static void main(String[] args) throws SQLException {
         Properties properties = new Properties();
@@ -19,9 +21,19 @@ public class DB {
         properties.setProperty("sslfactory", "org.postgresql.ssl.NonValidatingFactory");
         properties.setProperty("sslmode","prefer");
 
+        JFrame frame = new JFrame("Input for new user");
+        String fname = JOptionPane.showInputDialog(frame, "First name");
+        String lname = JOptionPane.showInputDialog(frame, "Last name");
+        String uid = JOptionPane.showInputDialog(frame, "UserId");
+        String em = JOptionPane.showInputDialog(frame, "email");
+        String pw = JOptionPane.showInputDialog(frame, "Password");
+
         Connection connection = DriverManager.getConnection(url,properties);
         Statement statement = connection.createStatement();
+        statement.executeUpdate("INSERT INTO \"HamelryckAxelWeb3\".person (userid, \"lastName\", \"firstName\", email, password) " +
+                "VALUES ('" + uid + "', '" + lname +"', '" + fname +"', '" + em+"' ,  '" +  pw +"')");
             ResultSet result = statement.executeQuery( "SELECT * FROM \"HamelryckAxelWeb3\".person");
+
 
         while(result.next()){
             String userid = result.getString("userid");
